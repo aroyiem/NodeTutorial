210eddafc443276772770aec1572397529dcfcdc
aroy <aroyiem@gmail.com>
2017-12-13 01:43:11 +0530

    Add files via upload

---
+++ NodeTutorial/httpServer/clientTest.js
@@ -0,0 +1,24 @@
+var http = require('http');
+
+// Options to be used by request 
+var options = {
+   host: 'localhost',
+   port: '8081',
+   path: '/index.html'  
+};
+
+// Make a request to the server
+var req = http.request(options, function(response){
+   // Continuously update stream with data
+   var body = '';
+   response.on('data', function(data) {
+      body += data;
+   });
+   
+   response.on('end', function() {
+      // Data received completely.
+      console.log(body);
+   });
+}
+);
+req.end();
